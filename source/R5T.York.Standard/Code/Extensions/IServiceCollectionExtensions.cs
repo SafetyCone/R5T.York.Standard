7f4ef9ac54412465e38de4b87063e8009c0f9cc9
MinexAutomation <minex.automation@gmail.com>
2019-12-29 02:07:29 -0800

    Added IServiceCollection extensions.

---
+++ source/R5T.York.Standard/Code/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.York.Newport;
+
+
+namespace R5T.York.Standard
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddHttpJsonSource(this IServiceCollection services)
+        {
+            services.AddSingleton<IHttpJsonSource, NewtonsoftHttpJsonSource>();
+
+            return services;
+        }
+    }
+}
